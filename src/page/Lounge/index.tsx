6ecd85232bb985be1ddb3c0174dbc902e15dd90d
yukina765578 <garden.students@gmail.com>
2023-11-21 16:04:36 +0900

    fixed map and positioning

--- src/page/Lounge/index.tsx
+++ src/page/Lounge/index.tsx
@@ -42,7 +42,7 @@ const Lounge = () => {
             <Popup
                 id={1}
                 viewportSize={viewportSize}
-                buttonPosition={{width: 9.18, height: 170.111 }}
+                buttonPosition={{width: 9.18, height: 177.469 }}
                 isOpen={(1 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='香音歩'
@@ -57,7 +57,7 @@ const Lounge = () => {
             <Popup
                 id={2}
                 viewportSize={viewportSize}
-                buttonPosition={{width: 9.18, height: 154.898 }}
+                buttonPosition={{width: 9.18, height: 159.369 }}
                 isOpen={(2 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='香音歩'
@@ -72,7 +72,7 @@ const Lounge = () => {
             <Popup
                 id={3}
                 viewportSize={viewportSize}
-                buttonPosition={{width: 9.18, height: 139.898 }}
+                buttonPosition={{width: 9.18, height: 141.369 }}
                 isOpen={(3 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='タイトル３'
@@ -87,7 +87,7 @@ const Lounge = () => {
             <Popup
                 id={4}
                 viewportSize={viewportSize}
-                buttonPosition={{width: 9.18, height: 124.898 }}
+                buttonPosition={{width: 9.18, height: 123.363  }}
                 isOpen={(4 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='タイトル4'
@@ -102,7 +102,7 @@ const Lounge = () => {
             <Popup
                 id={5}
                 viewportSize={viewportSize}
-                buttonPosition={{width: 9.18, height: 110.389 }}
+                buttonPosition={{width: 9.18, height: 105.379 }}
                 isOpen={(5 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='タイトル5'
@@ -117,7 +117,7 @@ const Lounge = () => {
             <Popup
                 id={6}
                 viewportSize={viewportSize}
-                buttonPosition={{width: 9.18, height: 94.408 }}
+                buttonPosition={{width: 9.18, height: 87.369 }}
                 isOpen={(6 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='タイトル6'
@@ -132,7 +132,7 @@ const Lounge = () => {
             <Popup
                 id={7}
                 viewportSize={viewportSize}
-                buttonPosition={{width: -9.18, height: 94.408 }}
+                buttonPosition={{width: -9.18, height: 87.369 }}
                 isOpen={(7 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='タイトル7'
@@ -147,7 +147,7 @@ const Lounge = () => {
             <Popup
                 id={8}
                 viewportSize={viewportSize}
-                buttonPosition={{width: -9.18, height: 110.389 }}
+                buttonPosition={{width: -9.18, height: 105.379 }}
                 isOpen={(8 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='タイトル8'
@@ -162,7 +162,7 @@ const Lounge = () => {
             <Popup
                 id={9}
                 viewportSize={viewportSize}
-                buttonPosition={{width: -9.18, height: 124.898 }}
+                buttonPosition={{width: -9.18, height: 123.363  }}
                 isOpen={(9 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='タイトル9'
@@ -177,7 +177,7 @@ const Lounge = () => {
             <Popup
                 id={10}
                 viewportSize={viewportSize}
-                buttonPosition={{width: -9.18, height: 139.898 }}
+                buttonPosition={{width: -9.18, height: 141.369 }}
                 isOpen={(10 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='タイトル10'
@@ -192,7 +192,7 @@ const Lounge = () => {
             <Popup
                 id={11}
                 viewportSize={viewportSize}
-                buttonPosition={{width: -9.18, height: 154.898 }}
+                buttonPosition={{width: -9.18, height: 159.369 }}
                 isOpen={(11 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='タイトル11'
@@ -207,7 +207,7 @@ const Lounge = () => {
             <Popup
                 id={12}
                 viewportSize={viewportSize}
-                buttonPosition={{width: -9.18, height: 170.111 }}
+                buttonPosition={{width: -9.18, height: 177.469 }}
                 isOpen={(12 === currentModal)}
                 modalImage={Morimoto}
                 modalTitle='タイトル12'
